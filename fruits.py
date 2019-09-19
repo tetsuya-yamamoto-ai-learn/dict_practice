b63891bcbfdf938965cf5f86c9e03059c07cb1ca
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-19 15:11:09 +0900

    fruits.pyの作成
    - 辞書型データのValueへのアクセス方法の確認

---
+++ fruits.py
@@ -0,0 +1,18 @@
+def main():
+    fruits_dict = {
+        'りんご' : 'Apple',
+        'ぶどう' : 'Grape',
+        'みかん' : 'Orange',
+    }
+
+    # Grape と出力するコードを書いてね
+    print(fruits_dict['ぶどう'])
+
+    # Orange と出力するコードを書いてね
+    print(fruits_dict['みかん'])
+
+    # Apple と出力するコードを書いてね
+    print(fruits_dict['りんご'])
+
+if __name__ == '__main__':
+    main()
